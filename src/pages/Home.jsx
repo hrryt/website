4e804bcfd383e5cb94746bd5645857f3b6fa11ae
hrryt <harry@mayesfield.uk>
2026-08-08 15:59:42 +0100

    Add Quadratic,CoordinateLabel graphing components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import Window from '../components/Window.jsx';
 import Graph from '../components/graphing/Graph.jsx';
 import Quadratic from '../components/graphing/Quadratic.jsx';
-import Label from '../components/graphing/Label.jsx';
+import CoordinateLabel from '../components/graphing/CoordinateLabel.jsx';
 
 export default function Home() {
+  const viewBox = [-5, -4, 7, 8];
   return (
     <main>
       <Window title="Home">
         <p>Hello World!</p>
         <div class="field-border kern">
-          <Graph viewBox={[-5, -1, 7, 6]}>
-            <Quadratic />
-            <Label x="-2" y="4" position="left">x²</Label>
+          <Graph viewBox={viewBox}>
+            <Quadratic a={2} b={1} c={-3} viewBox={viewBox} />
+            <CoordinateLabel x={-.25} y={-3.125} position="bottom" />
             <polygon points="1,2 -1,1, -4,3" />
-            <Label x="-4" y="3" position="top">(-4, 3)</Label>
+            <CoordinateLabel x={-4} y={3} position="top" />
           </Graph>
         </div>
       </Window>
