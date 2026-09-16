6c30d8c54fd6f495f02267574eb88b15d8352c7f
hrryt <harry@mayesfield.uk>
2026-09-16 18:13:03 +0100

    Upgrade http to https

--- serve.sh
+++ serve.sh
@@ -5,6 +5,6 @@ tar -c -f ../archive.tar *
 cd ..
 
 . ./.env
-curl "http://$2" -X PUT -H "Authorization: Pages $GIT_PAGES_PASSWORD" -H "Content-Type: application/x-tar" --data-binary @archive.tar
+curl "https://$2" -X PUT -H "Authorization: Pages $GIT_PAGES_PASSWORD" -H "Content-Type: application/x-tar" --data-binary @archive.tar
 
 rm archive.tar
